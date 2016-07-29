4a7a68c8f518b18bb6650ca35f2caf402e46728c
syuzyuzyu <syuzyuzyu@gmail.com>
2016-07-29 21:28:22 +0900

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,13 +29,26 @@ class User < ActiveRecord::Base
   end
 
     # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーログインを破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # アカウントを有効にする self.update_attributeだけどselfはいらない
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
@@ -50,5 +63,5 @@ class User < ActiveRecord::Base
      self.activation_token  = User.new_token
      self.activation_digest = User.digest(activation_token)
    end
-   
+
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -7,7 +7,7 @@ class UserMailerPreview < ActionMailer::Preview
      user = User.first
      user.activation_token = User.new_token
      UserMailer.account_activation(user)
-   end
+  end
 
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
